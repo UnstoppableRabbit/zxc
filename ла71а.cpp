0c8327b05d783b733dceb78ad5da5cfb341a6f12
UnstoppableRabbit <74426525+UnstoppableRabbit@users.noreply.github.com>
2020-11-13 22:07:47 +0300

    qwe

---
+++ ла71а.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> myfnc(vector <string> string);
+
+int main()
+{
+    int k = 0;
+    vector<string> str;
+    string a;
+    string word;
+    getline(cin, a);
+    for (int i = 0; i <= size(a); i++)
+    {
+        if (a[i] != ' ' && a[i] != '\0')
+            word.push_back(a[i]);
+        else
+        {
+            str.push_back(word);
+            word = "";
+            k++;
+        }
+    }
+    vector<string> nstr = myfnc(str);
+    for (int i = 0; i < size(nstr); i++)
+        cout << nstr[i] << " ";
+}
+vector<string> myfnc(vector <string> str)
+{
+    vector<string> nstr;
+    for (string x : str) {
+        int i = size(x) - 1;
+        for (int j = 0; j < i; j++, i--)
+        {
+            char t = x[j];
+            x[j] = x[i];
+            x[i] = t;
+        }
+        nstr.push_back(x);
+    }
+    return nstr;
+}
